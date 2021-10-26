2228dfea4074399947c474baba3ee5cebc86cb37
th3n3wguy1 <hacktheboxman@gmail.com>
2021-10-26 20:32:34 +0200

    update hello world

---
+++ src/contracts/L2HelloWorld.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 < 0.9.0;
+
+contract HelloWorld{
+
+    string value = 'Hello World!';
+
+    function get() public view returns(string memory){
+
+        return value;
+
+    }
+
+}
