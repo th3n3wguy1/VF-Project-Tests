08661b4e8b1850be3128b81e6a704150fada8ff3
th3n3wguy1 <hacktheboxman@gmail.com>
2021-10-27 17:08:29 +0200

    trying some api stuff

---
+++ src/contracts/Single-Swap-test.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 < 0.9.0;
+pragma abicoder v2;
+
+// Communication with UniswapAPI
+
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+
+contract SwapExcample{
+
+    ISwapRouter public immutable swapRouter;
+
+
+}
--- src/contracts/uniswapAPI-test.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 < 0.9.0;
-
-// Communication with UniswapAPI
-
