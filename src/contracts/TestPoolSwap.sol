8f4bae501e5ee8f588d076e8e63a5d3a174e2f0b
th3n3wguy1 <80165667+th3n3wguy1@users.noreply.github.com>
2021-10-31 16:45:51 +0100

    Dynamic functions added
    Still work to do

--- src/contracts/TestPoolSwap.sol
+++ src/contracts/TestPoolSwap.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.0;
 
 /*
 Erster Entwurf für Contract-Based-Swapping. Logik nicht mal ansatzweise final.
@@ -15,48 +16,46 @@ Erste Deposits in Contract selbst müssen von außerhalb (TypeSkript) erfolgen.
 
 */
 
-pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './dependencies/ISwapRouter.sol';
 import './dependencies/TransferHelper.sol';
 
-
+/** @author VF-Team
+ *  @title Volatility-Farming-Pool template 
+ */ 
 contract VFPool{
    
     address public constant DAI = 0xaD6D458402F60fD3Bd25163575031ACDce07538D;
     address public constant WETH9 = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
-  
+    uint24 public constant poolFee = 3000;
+
     ISwapRouter public immutable swapRouter;
-   
-    // Investoren dokumentieren. 
+  
+    mapping(address => Investor) private Investors;
+
+    address[] private InvestorAddre;
+    
     struct Investor{
         IERC20 deposit_token;
         uint256 deposit_amount;
     }
-    mapping(address => Investor) private Investors;
-    
-    // Später vlt verwenden um Input zu vereinfachen.
-    mapping(address => string) private Tokens;
-   
-    address[] private InvestorAddre;
-   
-    uint24 public constant poolFee = 3000;
+ 
 
-    constructor() {
-        buildTokenAddresses();
-        swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
-    }
-    
-    // Im Moment nur for Fun
-    function buildTokenAddresses() private{
+    /** Sets swap router
+     */
+    constructor(address _router) {
         
-        Tokens[DAI] = "DAI";
-        Tokens[WETH9] = "WETH9";
+        swapRouter = ISwapRouter(_router);
         
     }
     
-    // Neuen Investor erstellen. Muss beim Deposit (TypeSkript) zusätzlich aufgerufen werden
+    /** Adds a new investor to the memory stored stuct / mapping
+     *  @param address_ is the new investors address
+     *  @param token_ is the token, the investor deposited in the Contract
+     *  @param amount_ is the amount of token, he deposited in the contract
+     *  @dev adds the address to the InvestorAddre array
+     */
     function addInvestor(address address_, address token_, uint256 amount_) public{
         
         Investor storage investor = Investors[address_];
@@ -66,12 +65,12 @@ contract VFPool{
         InvestorAddre.push(address_);
     }
     
-    // Auszahlen von eingezahlten Token
+    
+    /**
+     * 
+     * 
+     */
     function withdraw(address _address, address _token, uint256 _amount) public payable {
-        
-        // Im Moment nur zu Test-zwecken auf einzelne Punkte geachtet. Nicht final
-        require(Investors[_address].deposit_token == IERC20(_token), "No balance under this token");
-        require(Investors[_address].deposit_amount >= _amount, "Not enough balance in Account");
         
         require(IERC20(DAI).balanceOf(address(this)) >= _amount, "Contract does currently not hold enough of this token");
         
@@ -82,60 +81,65 @@ contract VFPool{
     }
     
     
-    //Getter Funktionen
-    function getCapitalDAI() public view returns (uint256){
+    function getTokenCapital(address token) public view returns (uint256){
         
-        return IERC20(DAI).balanceOf(address(this));
-        
-    }
-   
-    function getCapitalWETH() public view returns (uint256){
-        
-        return IERC20(WETH9).balanceOf(address(this));
-        
+        return IERC20(token).balanceOf(address(this));
+    
     }
     
-    function getETHCapital() public view returns (uint256) {
-        
+    
+     function getInvestors() public view returns (address[] memory) {
+        return InvestorAddre;
+    }
+    
+    
+    function getEthAmount() public view returns (uint256) {
         return address(this).balance;
+    }
+    
+    
+    function investmentStrat() private pure returns (bool startTransfer, address tokenIN, address tokenOUT, uint256 amount){
+        
+        /*
+        bool:
+        TRUE, wenn swap ausgeführt werden soll
+        FALSE, wenn swap nicht ausgeführt werden soll
+        */
+        
+        return (true, address(0xaD6D458402F60fD3Bd25163575031ACDce07538D), address(0xc778417E063141139Fce010982780140Aa0cD5Ab), 1);
         
     }
     
-    // Hard-Coded Swap funktionen
-    function swapExactDaiToWETH(uint256 amountIn) external payable returns (uint256 amountOut) {
     
-        // Approve the router to spend DAI.
-        TransferHelper.safeApprove(DAI, address(swapRouter), amountIn);
-
-        // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
-        // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
-        ISwapRouter.ExactInputSingleParams memory params =
-            ISwapRouter.ExactInputSingleParams({
-                tokenIn: DAI,
-                tokenOut: WETH9,
-                fee: poolFee,
-                recipient: address(this),
-                deadline: block.timestamp,
-                amountIn: amountIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            });
-
-        // The call to `exactInputSingle` executes the swap.
-        amountOut = swapRouter.exactInputSingle(params);
+    // To Be executed by BOT
+    function executeCurrentInvestmentAdvices() public payable{
+        
+        (bool startTransfer, address tokenIN, address tokenOUT, uint256 amount) = investmentStrat();
+        
+        if (startTransfer == true) {
+            swapExcactInToOut(
+                amount,
+                tokenIN, 
+                tokenOUT
+                );
+        }
+        
+        // transferRewardsToExecutor();
+        
     }
     
-    function swapExactWethToDai(uint256 amountIn) external payable returns (uint256 amountOut) {
+    
+    function swapExcactInToOut(uint256 amountIn, address tokenIN, address tokenOUT) private returns (uint256 amountOut) {
     
         // Approve the router to spend DAI.
-        TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
+        TransferHelper.safeApprove(tokenIN, address(swapRouter), amountIn);
 
         // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
         // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
         ISwapRouter.ExactInputSingleParams memory params =
             ISwapRouter.ExactInputSingleParams({
-                tokenIn: WETH9,
-                tokenOut: DAI,
+                tokenIn: tokenIN,
+                tokenOut: tokenOUT,
                 fee: poolFee,
                 recipient: address(this),
                 deadline: block.timestamp,
@@ -148,3 +152,4 @@ contract VFPool{
         amountOut = swapRouter.exactInputSingle(params);
     }
 }
+
