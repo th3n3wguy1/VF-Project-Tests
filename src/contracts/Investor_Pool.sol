0516159a7f6e3e71362b6b9ffa1a952644a21244
stonkchonk <heilfred98@gmail.com>
2021-11-07 20:58:49 +0100

    added proper logging, cannot yet remove investor

--- src/contracts/Investor_Pool.sol
+++ src/contracts/Investor_Pool.sol
@@ -4,8 +4,14 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract InvestorPool {
     
+    //Events for logging purposes
+    event LogString(string _string);
+    event LogAddress(address _address);
+    event LogNumber(uint256 _number);
+    
     //all invested addresses
     address[] public investors;
+    uint256 public len;
     
     //address -> stake in pool
     //stake = 1'000'000'000'000'000'000 equals 100%, stake = 0 
@@ -22,7 +28,32 @@ contract InvestorPool {
         stakes[_address] = _stake;
     }
     
-    function removeInvestor(address _address) public {
+    function removeInvestorAddress(address _address) public {
+        //remove stake
         delete stakes[_address];
+        //remove address from stored addresses
+        address[] memory newInvestors;
+        for(uint i=0; i<investors.length; i++){
+            if(investors[i]!=_address){
+                newInvestors[i] = investors[i];
+            }
+            investors = newInvestors;
+        }
+    }
+    
+    
+    
+    function addInvestorAddress(address _address)public {
+        investors.push(_address);
+        _logStoredInvestorInformation();
+    }
+    
+    function _logStoredInvestorInformation()public {
+        for(uint i=0; i<investors.length; i++){
+            //log address
+            emit LogAddress( investors[i] );
+            //log corresponding stakes
+            emit LogNumber( stakes[ investors[i] ] );
+        }
     }
 }
