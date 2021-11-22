48afe3bf13a43a46a7190a09917d578862d0b62c
stonkchonk <heilfred98@gmail.com>
2021-11-22 22:01:19 +0100

    deposit stake handling works

--- src/contracts/Investor_Pool.sol
+++ src/contracts/Investor_Pool.sol
@@ -11,11 +11,15 @@ contract InvestorPool {
     
     //all invested addresses
     address[] public investors;
-    uint256 public len;
+    
+    //stake = 1'000'000'000'000'000'000 equals 100%, stake = 0 
+    uint256 totalSupply = 1000000000000000000;
     
     //address -> stake in pool
-    //stake = 1'000'000'000'000'000'000 equals 100%, stake = 0 
     mapping(address => uint256) public stakes;
+    
+    //minimum deposit 10'000'000'000'000'000 wei = 0.01 ether
+    uint256 minDeposit = 10000000000000000;
     
     
     //Funktionen werden noch korrekt implementiert
@@ -24,9 +28,6 @@ contract InvestorPool {
         return stakes[_address];
     }
     
-    function setInvestorStake(address _address, uint256 _stake) public {
-        stakes[_address] = _stake;
-    }
     
     function removeInvestorAddress(address _address) public{
         
@@ -72,12 +73,49 @@ contract InvestorPool {
         return false;
     }
     
+    function _sumInvestorStakes() private view returns (uint256){
+        uint256 sum = 0;
+        for(uint i=0; i<investors.length; i++){
+            sum = sum + stakes[investors[i]];
+        }
+        return sum;
+    }
+    
     function _logStoredInvestorInformation()public{
         for(uint i=0; i<investors.length; i++){
             //log address
             emit LogAddress( investors[i] );
             //log corresponding stakes
             emit LogNumber( stakes[ investors[i] ] );
+        }
+    }
+    
+    function recalculateAllStakesOnDeposit(uint256 amount) private {
+        uint256 oldBalance = this.balance() - amount;
+        for(uint i=0; i<investors.length; i++){
+            stakes[investors[i]] = (stakes[investors[i]]*oldBalance)/(oldBalance + amount);
+        }
+    }
+    
+    function balance() external view returns (uint256){
+        return address(this).balance;
+    }
+    
+    
+    function deposit() external payable{
+        //check if minimum deposit size applies, if yes abort transaction
+        require(msg.value >= minDeposit, "Min 0.01eth");
+        
+        //assign new stakes for everyone, add address to investors on first deposit
+        if(investors.length < 1){
+            addInvestorAddress(msg.sender);
+            stakes[msg.sender] = totalSupply;
+        }else{
+            if(!_investorsContainsAddress(msg.sender)){
+                addInvestorAddress(msg.sender);
+            }
+            recalculateAllStakesOnDeposit(msg.value);
+            stakes[msg.sender] = totalSupply - _sumInvestorStakes() + stakes[msg.sender];
         }
     }
 }
