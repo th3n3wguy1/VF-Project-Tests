75ea5e2374a75bbd2541fc28d8c3a3e07b032e16
stonkchonk <heilfred98@gmail.com>
2022-01-14 15:07:57 +0100

    strategy avg works

--- src/contracts/Strategy_Avg.sol
+++ src/contracts/Strategy_Avg.sol
@@ -4,8 +4,11 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Strategy_Avg {
     
+    //logger statement
+    event LogString(string);
+
     //Array of prices: volatile coin - stable coin
-    uint256[] prices = [10,20,30,40];
+    uint256[] prices = [10,20,30,40,30,60,12,34,35,38,23,49,23,25,30];
 
     //algo parameters, these can be fine tuned
 
@@ -15,12 +18,20 @@ contract Strategy_Avg {
     //minimum numer of price values to determine average from
     uint256 minPrices = 6;
 
+    //comparisson parameters
+    uint256 minDifferenceUp = 5;
+    uint256 minDifferenceDown = 5;
+
     /*
     Add new value to stored values.
     Execute trade evaluation.
     */
     function addMockValue(uint256 newPrice) public{
         prices.push(newPrice);
+        string memory recommendation;
+        recommendation = evaluateLatestMovement();
+        emit LogString(recommendation);
+
     }
 
     function avg(uint[] memory numberArray) private view returns (uint){
@@ -47,7 +58,49 @@ contract Strategy_Avg {
         }
     }
 
-    function log() public returns (uint[] memory){
-        return lastXValues(5, prices);
+    function compareLatestPriceToRelevantValues()private view returns (uint, bool){
+         uint[] memory relevantValues = lastXValues(lastXPrices, prices);
+         require(relevantValues.length >= minPrices, "Not enough data.");
+         uint latestPrice = prices[prices.length - 1];
+         uint relevantAverage = avg(relevantValues);
+         //actual comparisson here
+         uint difference = 0;
+         bool priceIsUp = false;
+         if(latestPrice >= relevantAverage){
+             difference = latestPrice - relevantAverage;
+             priceIsUp = true;
+         }else{
+             difference = relevantAverage - latestPrice;
+         }
+         //return difference and price movement
+         return (difference, priceIsUp);
+    }
+
+    //returns string: buy/sell/hold
+    function evaluateLatestMovement() private view returns (string memory){
+        bool priceIsUp;
+        uint difference;
+        (difference, priceIsUp) =  compareLatestPriceToRelevantValues();
+        if(priceIsUp){
+            if(difference >= minDifferenceUp){
+                //recommend sell
+                return "sell";
+            }else{
+                //recommend hold
+                return "hold";
+            }
+        }else{
+            if(difference >= minDifferenceDown){
+                //recommend buy
+                return "buy";
+            }else{
+                //recommend hold
+                return "hold";
+            }
+        }
+    }
+
+    function logValues() public returns (uint[] memory){
+        return prices;
     }
 }
